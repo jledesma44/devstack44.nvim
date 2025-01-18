00b324cd73720415bce1d25676366579a5c9b5ee
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-01-18 13:03:28 -0800

    added Goyo plugin and vim-pencil plugin options to initial in markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,8 @@
 vim.opt.spell = true
 vim.opt.number = false
+vim.cmd ':Goyo 80x80%'
+
+-- Check if Pencil is actually loaded before calling it
+if vim.fn.exists ':Pencil' == 2 then
+  vim.cmd 'PencilSoft'
+end
