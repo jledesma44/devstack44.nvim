a75e95eb791814ed0db811c8ab2254f72bfc3d5a
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-01-20 06:17:59 -0800

    Removed vim-pencil from the misc plugins file to its own lua file

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -42,18 +42,6 @@ return {
     end,
   },
   {
-    'preservim/vim-pencil',
-    lazy = false,
-    config = function()
-      vim.cmd [[runtime! plugin/pencil.vim]]
-      vim.g.tex_conceal = ''
-      vim.g['pencil#conseallevel'] = 0
-      vim.g['pencil#initConceal'] = 0
-      vim.g['pencil#wrapModeDefault'] = 'soft'
-      vim.cmd [[autocmd FileType markdown,mkd call pencil#init({'wrap', 'soft'})]]
-    end,
-  },
-  {
     'karb94/neoscroll.nvim',
     opts = {},
   },
