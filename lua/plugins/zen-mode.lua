f790fd4ad4783519ae2cad829f4f8e60667bd3d0
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-01-19 22:37:50 -0800

    Modified the options in Zen-mode.lua
    1. Backdrop = 1
    2. width = .50
    3. height = 0.90

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -2,13 +2,13 @@ return {
   'folke/zen-mode.nvim',
   opts = {
     window = {
-      backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+      backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
       -- height and width can be:
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
-      width = 120, -- width of the Zen window
-      height = 1, -- height of the Zen window
+      width = 0.50, -- width of the Zen window
+      height = 0.90, -- height of the Zen window
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
       options = {
@@ -30,7 +30,7 @@ return {
         showcmd = false, -- disables the command in the last line of the screen
         -- you may turn on/off statusline in zen mode by setting 'laststatus'
         -- statusline will be shown only if 'laststatus' == 3
-        laststatus = 0, -- turn off the statusline in zen mode
+        laststatus = 3, -- turn off the statusline in zen mode
       },
       twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
       gitsigns = { enabled = false }, -- disables git signs
@@ -41,13 +41,13 @@ return {
       -- - allow_remote_control socket-only
       -- - listen_on unix:/tmp/kitty
       kitty = {
-        enabled = false,
+        enabled = true,
         font = '+4', -- font size increment
       },
     },
     -- callback where you can add custom code when the Zen window opens
-    on_open = function(win) end,
+    --on_open = function(win) end,
     -- callback where you can add custom code when the Zen window closes
-    on_close = function() end,
+    --on_close = function() end,
   },
 }
