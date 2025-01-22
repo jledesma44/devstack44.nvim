816f5cdace879b10e3526d6ff1bc21b030c4230e
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-01-22 09:32:00 -0800

    removed and added back vim.cmd for PencilSoft plugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,7 +3,7 @@ vim.opt.number = false
 vim.opt.linebreak = true
 vim.opt.wrap = true
 --vim.opt.softwrap = true
---vim.cmd ':PencilSoft'
+vim.cmd ':PencilSoft'
 
 -- Check if Pencil is actually loaded before calling it
 --if vim.fn.exists 'Pencil' == 2 then
