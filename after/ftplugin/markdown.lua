fbceec1f8337758e718a8ba7590aea29035dc761
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-01-23 05:04:27 -0800

    ftplugin added command for pencilsoft plugin to markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,10 +2,6 @@ vim.opt.spell = true
 vim.opt.number = false
 vim.opt.linebreak = true
 vim.opt.wrap = true
+
 --vim.opt.softwrap = true
 vim.cmd ':PencilSoft'
-
--- Check if Pencil is actually loaded before calling it
---if vim.fn.exists 'Pencil' == 2 then
--- vim.cmd ':PencilSoft'
---end
