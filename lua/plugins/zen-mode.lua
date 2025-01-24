efb4d27a0dfc1c066a813262ea20edd4cc76471b
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-01-23 19:01:20 -0800

    Commented out settings for increasing font in the kitty config.

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -40,10 +40,10 @@ return {
       -- to make this work, you need to set the following kitty options:
       -- - allow_remote_control socket-only
       -- - listen_on unix:/tmp/kitty
-      kitty = {
-        enabled = true,
-        font = '+0', -- font size increment
-      },
+      --kitty = {
+      --enabled = true,
+      --font = '+0', -- font size increment
+      --},
     },
     -- callback where you can add custom code when the Zen window opens
     --on_open = function(win) end,
