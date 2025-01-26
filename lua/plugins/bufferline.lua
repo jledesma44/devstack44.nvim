674bf492a7571e90cfda56eb43aa5f58e91174c4
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-01-25 20:55:05 -0800

    added and offset config to the bufferline.lua plugin

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,7 +7,15 @@ return {
   config = function()
     require('bufferline').setup {
       options = {
-        mode = 'buffers', -- set to "tabs" to only show tabpages instead
+        mode = 'buffers',
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Explorer',
+            --highlight = 'Directory',
+            separator = false,
+          },
+        }, -- set to "tabs" to only show tabpages instead
         themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
         numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
         close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
@@ -27,7 +35,7 @@ return {
         show_buffer_close_icons = true,
         show_close_icon = true,
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        separator_style = { '│', '│' }, -- | "thick" | "thin" | { 'any', 'any' },
+        separator_style = { '|', '|' }, -- | "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = true,
         always_show_bufferline = true,
         show_tab_indicators = false,
