5034eb06cb6077db3495fa135c0742ce044d1969
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-03-07 09:53:06 -0800

    Added spelling options to the options.lua file

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -41,6 +41,8 @@ vim.opt.shortmess:append 'c' -- Don't give |ins-completion-menu| messages (defau
 vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include '-')
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
